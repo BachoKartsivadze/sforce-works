dbbd26007577261615b66c65b4d0eb7cfeda0b32
unknown <bachoqarcivadze62@gmail.com>
2024-05-02 00:46:59 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+Random rnd = new Random();
+int guessNumber = 0;
+string hint = "";
+Console.WriteLine("input range minimum");
+int lower =  Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input range maximum");
+int higher =  Convert.ToInt32(Console.ReadLine());
+while(true) {
+    guessNumber = (higher + lower)/2;
+    Console.WriteLine($"is your number {guessNumber}?");
+    Console.WriteLine($"write {"yes"}, {"<"} or {">"} ");
+    hint = Console.ReadLine();
+    if (hint == "yes") {
+        Console.WriteLine("END");
+        return;
+    }
+    if (hint == "<") {
+        higher = guessNumber-1;
+    }
+    if (hint == ">") {
+        lower = guessNumber+1;
+    }
+
+}
